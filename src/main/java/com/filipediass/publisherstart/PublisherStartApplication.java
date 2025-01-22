8f98b7708c5cfcc7ac1ba7b2aab5f870f186bcb5
Filipe Dias <diasjoaum@gmail.com>
2025-01-22 09:15:46 -0300

    Initial commit

---
+++ src/main/java/com/filipediass/publisherstart/PublisherStartApplication.java
@@ -0,0 +1,13 @@
+package com.filipediass.publisherstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherStartApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(PublisherStartApplication.class, args);
+    }
+
+}
